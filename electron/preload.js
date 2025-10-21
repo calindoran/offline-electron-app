9c589b9cdff45c553934c5ce061b75cf11ec465a
Calin Doran <calindoran@gmail.com>
2025-10-21 20:48:22 +0100

    refactor: remove main and preload scripts for Electron app

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    performSync: (pendingMutations) => electron_1.ipcRenderer.invoke('perform-sync', pendingMutations),
-    on: (channel, cb) => {
-        electron_1.ipcRenderer.on(channel, (_event, ...args) => cb(...args));
-    }
-});
